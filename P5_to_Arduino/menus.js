14f652a0d01822b89ea581406e9ceb8fcfbf3217
Viviana <40200339@esmad.ipp.pt>
2024-01-22 00:58:16 +0000

    added general structure for hand calibrations menu

--- P5_to_Arduino/menus.js
+++ P5_to_Arduino/menus.js
@@ -4,9 +4,9 @@ document.querySelector('#mainMenu').style.width = window.innerWidth + 'px';
 document.querySelector('#mainMenu').style.height = window.innerHeight + 'px';
 
 /* dimensions for calibrationScreen div */
-document.querySelector('#calibrationScreen').style.width = window.innerWidth*0.9 + 'px';
-document.querySelector('#calibrationScreen').style.height = (window.innerHeight-window.innerWidth*0.1) + 'px';
-document.querySelector('#calibrationScreen').style.padding = window.innerWidth*0.05 + 'px';
+document.querySelector('#calibrationScreen').style.width = window.innerWidth + 'px';
+document.querySelector('#calibrationScreen').style.height = window.innerHeight*0.8 + 'px';
+document.querySelector('#calibrationScreen').style.paddingTop = window.innerHeight*0.1 + 'px';
 
 /* dimensions for play button */
 document.querySelector('#btnPlay').style.width = window.innerWidth*0.25 + 'px';
@@ -23,6 +23,19 @@ document.querySelector('#btnJoystick').style.height = window.innerWidth*0.1 + 'p
 document.querySelector('#btnHand').style.width = window.innerWidth*0.1 + 'px';
 document.querySelector('#btnHand').style.height = window.innerWidth*0.1 + 'px';
 
+/* dimensions for return button */
+document.querySelector('#btnReturn').style.width = window.innerWidth*0.1 + 'px';
+document.querySelector('#btnReturn').style.height = window.innerWidth*0.025 + 'px';
+document.querySelector('#btnReturn').style.left = window.innerWidth*0.025 + 'px';
+document.querySelector('#btnReturn').style.top = window.innerWidth*0.025 + 'px';
+
+/* dimensions for line calibration divs */
+document.querySelector('#divLines').style.width = window.innerWidth*0.8 + 'px';
+document.querySelector('#divLines').style.marginTop = window.innerHeight*0.5 + 'px';
+document.querySelector('#lineLeft').style.width = window.innerWidth*0.2 + 'px';
+document.querySelector('#lineRight').style.width = window.innerWidth*0.2 + 'px';
+
+
 
 //* event listener for buttons */
 /* clicking on the play button will hide the main menu */
@@ -36,4 +49,12 @@ document.querySelector('#btnHand').addEventListener('click', e => {
     document.querySelector('#calibrationScreen').style.display = 'flex';
 
 
+})
+
+/* clicking on the return button will take the player back to the main menu */
+document.querySelector('#btnReturn').addEventListener('click', e => {
+    document.querySelector('#mainMenu').style.display = 'flex';
+    document.querySelector('#calibrationScreen').style.display = 'none';
+
+    
 })
